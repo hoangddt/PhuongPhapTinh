213122fbd7fc8df030e9b16f75d5d543c4f336bb
Quoc Hoang <quochoangddt@gmail.com>
2015-03-10 02:36:41 +0700

    Add SoDoHoocnerTongQuat

---
+++ ChuongII-SoDoHoocnerTongQuat.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void Nhap(float a[], int n)
+{
+	printf("Nhap lan luot cac he so:\n");
+	for (int i = 0; i <= n; ++i)
+	{
+		scanf("%f", &a[i]);
+	}
+}
+
+void Hoocner(float a[], int n, float c)
+{
+ 	for (int i = n; i > 0; --i)
+	 	for (int j=0; j<i; j++)
+	 		a[j+1] = a[j]*c + a[j+1];
+}
+
+void KetQua(float a[], int n, float c)
+{
+	printf("Cac he so cua phuong trinh\n");
+	for (int i = 0; i <= n; ++i)
+	{
+		printf("%.2f  ", a[i]);
+	}
+}
+
+int main()
+{
+	int n, c;
+	float a[100];
+	printf("Nhap bac phuong trinh: n= ");
+	scanf("%d",&n);
+	Nhap(a,n);
+	printf("Nhap gia tri c: \n");
+	scanf("%d", &c);
+	Hoocner(a, n, c);
+	KetQua(a,n,c);
+}
