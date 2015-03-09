e73192df50edd2472f10b2e57aa51ddb999c9a21
Quoc Hoang <quochoangddt@gmail.com>
2015-03-10 02:41:55 +0700

    Add Recurison for Hoocner

--- ChuongII-SoDoHoocnerTongQuat.cpp
+++ ChuongII-SoDoHoocnerTongQuat.cpp
@@ -9,6 +9,16 @@ void Nhap(float a[], int n)
 	}
 }
 
+// Ham tinh dung de quy
+void HoocnerRecurison(float a[], int n, float c)
+{
+	if (n==0) return;
+	for (int i = 0;i < n;i++)
+		a[i+1] = a[i] *c +a[i+1];
+	HoocnerRecurison(a,n-1,c);
+}
+
+// Ham tinh khong dung de quy
 void Hoocner(float a[], int n, float c)
 {
  	for (int i = n; i > 0; --i)
@@ -34,6 +44,6 @@ int main()
 	Nhap(a,n);
 	printf("Nhap gia tri c: \n");
 	scanf("%d", &c);
-	Hoocner(a, n, c);
+	HoocnerRecurison(a, n, c);
 	KetQua(a,n,c);
 }
