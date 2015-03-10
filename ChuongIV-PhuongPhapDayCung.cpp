6422833c2ed28ae04c19dab5bcabd430bbc7bd85
Quoc Hoang <quochoangddt@gmail.com>
2015-03-10 11:42:50 +0700

    Add PhuongPhapDayCung

---
+++ ChuongIV-PhuongPhapDayCung.cpp
@@ -0,0 +1,69 @@
+// ChuongIV-Phuong phap Day Cung.
+// phuong trinh trang 24
+#include <iostream>
+#include <cmath>
+#define EPS 0.001
+using namespace std;
+
+// f(x) = 2^x + x -4
+float f(float x)
+{
+	return pow(2,x) + x -4;
+}
+
+
+void thongBaoKetQua(float kq, bool coNghiem = true)
+{
+	if (coNghiem)
+		cout<<"Phuong trinh co nghiem la: "<<kq;
+	else cout<<"Phuong trinh khong co nghiem trong khoang nay";
+}
+
+
+float Tinh(float a, float b)
+{
+	float temp,
+		  fx,
+		  x = 0,
+		  fa = f(a),
+		  fb = f(b);
+	do
+	{
+		temp = x;
+		x = a - (b-a)*fa/(fb - fa);
+		fx = f(x);
+
+		if (fa*fx < 0)
+		{
+			b = x;
+			fb = f(b);
+		}
+		if (fb*fx<0)
+		{
+			a = x;
+			fa = f(a);
+		}
+		// Xu ly lap vo han
+		if (x>b || x< a)
+		{
+			thongBaoKetQua(0, false);
+			return 0;
+		}
+		// lan dau tien chi co 1 x nen chua kiem tra dieu kien
+		if (temp == 0) continue;
+	}
+	while ( fabs(temp - x) > EPS );
+
+	thongBaoKetQua(x);
+ 	return x;
+
+}
+
+int main()
+{
+	float a, b;
+	cout<< "Nhap khoang nghiem [a, b]: ";
+	cin>>a>>b;
+	Tinh(a,b);
+	return 0;
+}
