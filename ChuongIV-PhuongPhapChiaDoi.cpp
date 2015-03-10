839d43f31e5a8222f6acd49db896fb60bb5d7d03
Quoc Hoang <quochoangddt@gmail.com>
2015-03-10 10:12:30 +0700

    Improve flow of PhuongPhapChiaDoi

--- ChuongIV-PhuongPhapChiaDoi.cpp
+++ ChuongIV-PhuongPhapChiaDoi.cpp
@@ -10,18 +10,43 @@ float f(float x)
 	return pow(2,x) + x -4;
 }
 
-int main()
+void thongBaoKetQua(float kq, bool coNghiem = true)
 {
-	float a,b,c;
-	printf("Nhap 2 diem a va b voi f(a)<0 va f(b)>0:\n");
-	scanf("%f %f", &a, &b);
+	if (coNghiem)
+		printf("Nghiem cua phuong tring la: %0.3f\n", kq);
+	else printf("Phuong trinh khong co nghiem o khoang nay.\n");
+}
+
+
+float Tinh(float a, float b)
+{
+	int count = 0;
+	float c;
 	do
 	{
 		c = 0.5*(a+b);
 		if (f(c)>0) b = c;
 		else a = c;
+		// xu ly lap vo han
+		count++;
+		if (count>1000) 
+		{
+			thongBaoKetQua(c, false);
+			return 0;
+		}
 	}
-	while ( fabs(a-b)>EPS && f(c) != 0);
-	printf("Nghiem cua phuong tring la: %0.3f\n", c);
+	while ( fabs(f(c)) > EPS);
+	thongBaoKetQua(c);
+	return c;
+}
+
+
+int main()
+{
+	float a,b;
+	printf("Nhap 2 diem a va b voi f(a)<0 va f(b)>0:\n");
+	scanf("%f %f", &a, &b);
+	Tinh(a,b);
+	
 	return 0;
 }
